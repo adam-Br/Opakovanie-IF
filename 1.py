7d861dd4b2f437f26aea1d583c4c7fe80b69f3b4
Adam <brizakadam@gmail.com>
2025-09-27 17:43:01 +0200

    1.

---
+++ 1.py
@@ -0,0 +1,9 @@
+a = int(input("prvé čislo: "))
+b = int(input("druhé čislo: "))
+
+if a * b == 100:
+    print("rovná sa 100")
+elif a * b > 100:
+    print("presiahol 100")
+else:
+    print("nepresiiahol 100")
